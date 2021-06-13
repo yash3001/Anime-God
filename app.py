be0b54c53372ac97a31816b116ba2ce818a25214
Yash Chauhan <yashchauhan197@gmail.com>
2021-06-13 17:53:07 +0530

    added some comments for future

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
+#scrapping the anime names after searching
 url = "https://gogoanime.vc//search.html?keyword="
+
 name = input("Enter the name of the anime: ")
 url = url+name
+
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'html5lib')
-
 names = soup.find("div", class_="last_episodes").find("ul", class_="items").find_all("li")
+
+#quiting if no anime for the given name found
 if(not len(names)):
     exit
+
+#getting the specific season or type
 d = {}
 index = 1
 for name in names:
@@ -20,12 +26,12 @@ for name in names:
 
 num = input("\nEnter your choice: ")
 
+#scrapping the specific anime for episode selection
 url = "https://gogoanime.vc"+d[int(num)]
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content, "html5lib")
 
-# print(soup.prettify)
 episodes = soup.find("div", class_="anime_video_body").find("ul").find("li").find("a")
 ep_start = int(episodes['ep_start'])
 ep_end = int(episodes['ep_end'])
@@ -36,22 +42,20 @@ series_name = url.split('/')[-1]
 
 url = f"https://gogoanime.vc/{series_name}-episode-{ep_num}"
 
-
+#downloading / streaming the anime episode
 r = requests.get(url)
 soup = BeautifulSoup(r.content, "html5lib")
 
 download_page_link = soup.find("div", class_="download-anime").find("ul").find_all("li")[0].find('a')['href']
-#print(download_page_link)
 
 url = download_page_link
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content, "html5lib")
-#print(soup.prettify)
 
 divs = soup.find_all("div", class_="mirror_link")
-# print(len(divs))
 
+#getting the resolution
 d = {}
 index = 1
 for div in divs[:-1]:
@@ -66,6 +70,7 @@ for div in divs[:-1]:
 # url = d[int(quality_num)]
 url = d[1]
 
+#running the anime on mpv
 import os
 
 cmd = f"mpv {url}"
