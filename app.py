98e466659e7eceab4ce063ce34075d00ea31d907
Yash Chauhan <yashchauhan197@gmail.com>
2021-06-14 14:01:10 +0530

    some minor changes

--- app.py
+++ app.py
@@ -68,36 +68,49 @@ ep_num = input(colored(f"\nChoose an episode({ep_start+1}-{ep_end}): ", 'yellow'
 
 series_name = url.split('/')[-1]
 
-url = f"https://gogoanime.vc/{series_name}-episode-{ep_num}"
+while True:
+    os.system('clear')
+    print(banner)
+    print(colored(f"\nCurrent Episode Number: {ep_num}", 'green'))
+    print(colored(f"Total Episodes: {ep_end}", 'green'))
+    url = f"https://gogoanime.vc/{series_name}-episode-{ep_num}"
 
-#downloading / streaming the anime episode
-r = requests.get(url)
-soup = BeautifulSoup(r.content, "html5lib")
+    #downloading / streaming the anime episode
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html5lib")
 
-download_page_link = soup.find("div", class_="download-anime").find("ul").find_all("li")[0].find('a')['href']
+    download_page_link = soup.find("div", class_="download-anime").find("ul").find_all("li")[0].find('a')['href']
 
-url = download_page_link
+    url = download_page_link
 
-r = requests.get(url)
-soup = BeautifulSoup(r.content, "html5lib")
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html5lib")
 
-divs = soup.find_all("div", class_="mirror_link")
+    divs = soup.find_all("div", class_="mirror_link")
 
-#getting the resolution
-d = {}
-index = 1
-for div in divs[:-1]:
-    qualities = div.find_all('div')
-    for quality in qualities:
-        q=quality.find('a')
-        # print(index, q.text.split()[1][1:])
-        d[index] = q['href']
-        index += 1
+    #getting the resolution
+    d = {}
+    index = 1
+    for div in divs[:-1]:
+        qualities = div.find_all('div')
+        for quality in qualities:
+            q=quality.find('a')
+            # print(index, q.text.split()[1][1:])
+            d[index] = q['href']
+            index += 1
 
-# quality_num = input("Choose the quality: ")
-# url = d[int(quality_num)]
-url = d[1]
+    # quality_num = input("Choose the quality: ")
+    # url = d[int(quality_num)]
+    url = d[1]
 
-#running the anime on mpv
-cmd = f"mpv {url}"
-os.system(cmd)
+    #running the anime on mpv
+    cmd = f"mpv {url} > /dev/null 2>&1"
+    os.system(cmd)
+    ans = input(colored("\nNext Episode? (y/n): ", 'yellow'))
+    if(ans == 'n' or ans == 'N'):
+        print(colored("Quitting, bye", 'red'))
+        break
+    if(int(ep_num) >= ep_end):
+        print(colored("Quitting, no more episodes in the current season", 'red'))
+        break
+    ep_num = int(ep_num) + 1
